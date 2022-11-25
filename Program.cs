777787b64454b68913325daf9ffb126b78e5e682
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-11-25 16:12:31 +0700

    continue the last method after create for and make new if and else for the node present

--- Program.cs
+++ Program.cs
@@ -21,7 +21,16 @@ namespace Exercise_Linked_List_A
         public bool Search(int rollNo, ref Node previous, ref Node current)
         /*Searches for the specified node*/
         {
-            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next) ;
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next);
+            {
+                if (rollNo == current.rollNumber)
+                    return (true); /*returns true if the node is found*/
+            }
+            if (rollNo = LAST.rollNumber)/* If the node is present at the end*/
+                return true;
+            else
+                return(false);/*returns false ifthe node is not found*/
+
         }
     }
 }
