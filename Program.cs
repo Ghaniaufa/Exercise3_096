ca50b1210068562a9747d12c7b9e17c3fb7b1e70
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-11-25 16:20:33 +0700

    create public void traverse for all the node list

--- Program.cs
+++ Program.cs
@@ -38,5 +38,24 @@ namespace Exercise_Linked_List_A
             else
                 return false;
         }
+        public void traverse()/*Traverse all the nodes of the list*/
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+            {
+                Console.Write("\nRecord in ThreadExceptionEventArgs list are:\n");
+                Node currentNode;
+                currentNode = LAST.next;
+                while (currentNode != LAST)
+                {
+                    Console.Write(currntNode.rollNumber + "   " + currentNode.name + "\n");
+                    currentNode = currentNode.next; 
+                }
+                Console.Write(LAST.rollNumber + "   " + LAST.name + "\n");
+            }
+        }
+
+
     }
 }
