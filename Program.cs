8c23144936e8dfe523a13f5fcb2199fb219c3c99
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-11-25 16:06:47 +0700

    Create 3 list for thecircular nexted list

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace Exercise_Linked_List_A
+{
+    class Node
+    {
+        /*Creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+}
